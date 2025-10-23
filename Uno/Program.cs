428a922f316611b216abd1f08830543c47805b3d
Tim Jonsson <tjonsson02@gmail.com>
2025-10-23 14:16:09 +0200

    Fixade samtliga kortklasser och effekter. Gjort så vi kan skapa ett kort.

--- Uno/Program.cs
+++ Uno/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using Uno;
 
 namespace Uno
 {
@@ -7,6 +8,17 @@ namespace Uno
         static void Main(string[] args)
         {
             Console.WriteLine("Han tar en nuuuuus");
+            var card = new NumberCard("red", "5");
+            var card1 = new SkipCard("blue");
+            var card2 = new ReverseCard("yellow");
+            var card3 = new PlusTwoCard("green");
+            var card4 = new PlusFourCard();
+            var card5 = new ChooseColorCard();
+            Console.WriteLine();
+
+
+
+
         }
     }
 }
--- Uno/UnoCard.cs
+++ Uno/UnoCard.cs
@@ -8,7 +8,15 @@ namespace Uno
 {
     internal abstract class UnoCard
     {
-        private string Color { get; }
-        private ICardEffect effect;
+        protected string Color { get; }
+        public string Symbol { get; }
+        private ICardEffect Effect;
+
+        protected UnoCard(string color, string symbol, ICardEffect effect)
+        {
+            Color = color;
+            Symbol = symbol;
+            Effect = effect;
+        }
     }
 }
