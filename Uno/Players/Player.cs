186d88f2be7297236c1e49f464a185152169fb8e
Linus <Linus@LINUS-PC>
2025-10-31 16:30:06 +0100

    masterändringar

---
+++ Uno/Players/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.Cards;
+using Uno.Player;
+
+namespace Uno.Player
+{
+    internal abstract class Player
+    {
+        public PlayerHand Hand { get; }
+        public string Name { get; }
+        private Deck Deck { get; }
+        protected IStrategy strategy { get; }
+
+        public Player(string name, IStrategy strategy)
+        {
+            Name = name;
+            this.strategy = strategy;
+            Hand = new PlayerHand();
+            makeHand();
+        }
+
+        private void makeHand()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Hand.AddCard(Deck.drawCard());
+            }
+        }
+        private void DrawCard() => Hand.AddCard(Deck.drawCard());
+
+        public abstract void playCard(UnoCard card);
+}
+
