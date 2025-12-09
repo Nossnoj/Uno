721abf961a88747b28d8257e78f206bd2a1b4edc
Linus <Linus@LINUS-PC>
2025-12-09 16:37:22 +0100

    Fixat fabriken enligt kravet

--- Uno/Factories/PlayerFactory.cs
+++ Uno/Factories/PlayerFactory.cs
@@ -8,13 +8,13 @@ using Uno.Players;
 
 namespace Uno.Factories
 {
-    internal abstract class PlayerFactory
+    internal interface IPlayerFactory
     {
         //KRAV 4
         //1: Factory Method Pattern
         //2: Vi har en abstrakt klass PlayerFactory och två konkreta subtyper; HumanPlayerFactory och AIPlayerFactory. Vi använder dessa för att skapa spelare i gameklassen.
         //3: Syftet med våran detta är att när vi skapar spelare så behöver vi inte veta hur. Game vet bara att det finns en spelare och behöver inte bry sig vilken typ eller hur den ska skapas.
         //Vi separerar skapandet och användningen av objektet. Detta ger oss möjlighet att lägga till flera typer av spelare utan att ändra någon logik. 
-        public abstract Player createPlayer(string name, IStrategy strategy, Deck deck, GameState state);
+        public Player createPlayer(string name, Deck deck, GameState state);
     }
 }
